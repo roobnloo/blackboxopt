1bb6e520659bf968c6fcf1be292560d64dbf7b78
Robin Liu <71776622+roobnloo@users.noreply.github.com>
2022-02-18 20:55:01 -0800

    exemplary plots

--- main.jl
+++ main.jl
@@ -1,13 +1,17 @@
 include("BlackBoxOptimization.jl")
-using .BlackBoxOptimization, Plots, LinearAlgebra, Random
+using .BlackBoxOptimization, Plots, LinearAlgebra, Random, Plots.Measures
 
 function flower(x; a=1, b=1, c=4)
     return a*norm(x) + b*sin(c*atan(x[2], x[1]))
 end
 
-Random.seed!(101)
-cma_result = covariance_matrix_adaptation(flower, [1,1], 6; m = 10)
-cemvn_result = cross_entropy_mvn(flower, [1,1], Matrix(1.0I, 2, 2), 6, 10, 5)
+μ0 = [2, 2]
+
+Random.seed!(102)
+cma_result = covariance_matrix_adaptation(flower, μ0, 12; m = 20)
+
+Random.seed!(102)
+cemvn_result = cross_entropy_mvn(flower, μ0, Matrix(1.0I, 2, 2), 6, 20, 10)
                             #k_max = 6, m = 10, m_elite=5)
 
 x = range(-3, 3, length = 1000)
@@ -28,13 +32,13 @@ function plot_update(result::IterResult, h::Plots.Plot{Plots.GRBackend})
                     xlim = (-3, 3), ylim = (-3, 3),
                     c = :white, aspect_ratio = :equal,
                     size = (500, 500), legend=:none)
-    return scatter(cont, first.(s), last.(s), legend=:none)
+    return scatter(cont, first.(s), last.(s), legend=:none, c = :white)
 end
 
-cma_plots = [plot_update(cma_result[2][i], heat) for i in 1:6]
-plot(cma_plots..., layout = (2, 3), legend = false,
-        size = (1200, 800))
+cma_plots = [plot_update(cma_result[2][i], heat) for i in 1:12]
+display(plot(cma_plots..., layout = (3, 4), legend = false,
+        size = (1200, 900), axis=([], false), margin = 0mm))
 
 cemvn_plots = [plot_update(cemvn_result[2][i], heat) for i in 1:6]
-plot(cemvn_plots..., layout = (2, 3), legend = false,
-        size = (1200, 800))
+display(plot(cemvn_plots..., layout = (2, 3), legend = false,
+        size = (900, 600), axis=([], false), margin = 0mm))
